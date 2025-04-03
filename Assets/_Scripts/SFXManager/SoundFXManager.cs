ea7dfbdbdb1a17b71ade1b0195ed6fc121df90c5
John-Perdix <joao_perdiz@outlook.com>
2025-04-03 16:48:23 +0100

    SoundFXManager scripts and 3d models

---
+++ Assets/_Scripts/SFXManager/SoundFXManager.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundFXManager : MonoBehaviour
+{
+    public static SoundFXManager instance;
+    [SerializeField] private AudioSource soundFXObject;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance == null){
+            instance = this;
+        }
+    }
+
+    // Update is called once per frame
+    public void PlaySoundFXClip(AudioClip audioClip, Transform spawnTransform, float volume)
+    {
+        //spawn the gameobject
+        AudioSource audioSource = Instantiate(soundFXObject, spawnTransform.position, Quaternion.identity);
+
+        //assign the audioclip
+        audioSource.clip = audioClip;
+
+        //assign volume
+        audioSource.volume = volume;
+
+        //play sound
+        audioSource.Play();
+
+        //get length of clip
+        float clipLength = audioSource.clip.length;
+
+        //destroy the clip after played
+        Destroy(audioSource.gameObject, clipLength);
+        
+    }
+
+
+    public void PlayRandomSoundFXClip(AudioClip[] audioClip, Transform spawnTransform, float volume, float minPitch = 1f, float maxPitch = 1f)
+    {
+        //random index
+        int rand = Random.Range(0, audioClip.Length);
+        //spawn the gameobject
+        AudioSource audioSource = Instantiate(soundFXObject, spawnTransform.position, Quaternion.identity);
+
+        //assign the audioclip
+        audioSource.clip = audioClip[rand];
+
+        //assign volume
+        audioSource.volume = volume;
+
+        //assign random pitch
+        audioSource.pitch = Random.Range(minPitch, maxPitch);
+
+        //play sound
+        audioSource.Play();
+
+        //get length of clip
+        float clipLength = audioSource.clip.length;
+
+        //destroy the clip after played
+        Destroy(audioSource.gameObject, clipLength);
+        
+    }
+}
---
+++ Assets/_Scripts/SFXManager/SoundMixerManager.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundMixerManager : MonoBehaviour
+{
+    [SerializeField] private AudioMixer audioMixer;
+
+    public void SetMasterVolume(float level){
+        audioMixer.SetFloat("MasterVolume", MathF.Log10(level) *20f);
+    }
+
+    public void SetSoundFXVolume(float level){
+        audioMixer.SetFloat("SoundFXVolume", MathF.Log10(level)*20f);
+    }
+
+    public void SetMusicVolume(float level){
+    audioMixer.SetFloat("MusicVolume", MathF.Log10(level)*20f);
+    }
+
+
+}
